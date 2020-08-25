ff4d33f7158fa3f0db2814408046df8e45a1e212
Cloudbop <boppincolin@gmail.com>
2020-08-26 00:56:50 +0100

    enqued theme assets

--- functions.php
+++ functions.php
@@ -5,3 +5,16 @@
  * @package cgr-awpt
  * 
  */
+
+ function cgr_awpt(){
+   // - use this for timestamp, only changes if file is modified
+   $tmp_dir = get_template_directory() . '/style.css';
+   //
+   wp_enqueue_style($handle = 'styles-css', get_stylesheet_uri(), [], $ver = filemtime($tmp_dir), $media= 'all');
+   //
+   $tmp2 = get_template_directory() . '/assets/main.js';
+   wp_enqueue_script($handle = 'main-js', get_template_directory_uri().'/assets/main.js', [], $ver = filemtime($tmp2), $in_footer=true);
+ 
+}
+
+ add_action('wp_enqueue_scripts', 'cgr_awpt');
