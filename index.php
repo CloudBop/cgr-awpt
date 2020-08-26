6e0f2010700d3d4229f20fc0589f6d92bfaf92da
Cloudbop <boppincolin@gmail.com>
2020-08-26 22:34:32 +0100

    implemented 3 col grid with BS for blog post loop

--- index.php
+++ index.php
@@ -16,18 +16,52 @@
     <?php if ( have_posts() ) { ?>
       
       <div class="container">
-        <?php 
-          while ( have_posts() ) : the_post();
 
-            the_title();
-            the_content();
+        <?php if( is_home() && ! is_front_page() ) { ?>
 
-          endwhile;
-        ?>
-      </div>
+          <header class="mb-5">
+              <h1 class="page-title screen-reader-text">
+                <?php single_post_title(); ?>
+              </h1>
+          </header>
+        
+        <?php } // endif ?>
+
+        <div class="row">
+          <?php 
+
+            $index=0;
+            $no_of_columns=3;
+
+
+            while ( have_posts() ) : the_post();
+
+              if ( 0 === $index % $no_of_columns ) {
+                ?>
+                  <div class="col-lg-4 col-md-6 col-12">
+                  
+                <?php
+              }
+
+              ?>
+                <h3><?php the_title(); ?></h3>
+                <div><?php the_excerpt(); ?></div>
+                <span><?php echo $index; ?>  </span>
+              <?php
+              $index++;
+              if ( 0 !== $index && 0 === $index % $no_of_columns) {
+                ?>
+                  </div>
+
+              <?php }
+              
+              endwhile;
+              ?>
+        </div>
+        
+      </div>  <!-- end container -->
     
     <?php }?>
-
 
   </main>
 </div>
