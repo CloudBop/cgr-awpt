e084d90cc2e5015d29145d79cfa406a7bd5c664a
Cloudbop <boppincolin@gmail.com>
2020-08-26 01:02:36 +0100

    updated enqueue to use register. recomended

--- functions.php
+++ functions.php
@@ -7,14 +7,17 @@
  */
 
  function cgr_awpt(){
-   // - use this for timestamp, only changes if file is modified
-   $tmp_dir = get_template_directory() . '/style.css';
-   //
-   wp_enqueue_style($handle = 'styles-css', get_stylesheet_uri(), [], $ver = filemtime($tmp_dir), $media= 'all');
-   //
-   $tmp2 = get_template_directory() . '/assets/main.js';
-   wp_enqueue_script($handle = 'main-js', get_template_directory_uri().'/assets/main.js', [], $ver = filemtime($tmp2), $in_footer=true);
- 
+  // - use this for timestamp, only changes if file is modified
+  $tmp_dir = get_template_directory() . '/style.css';
+  $tmp2 = get_template_directory() . '/assets/main.js';
+  //
+  // - register with WP :- can enqueue programatically, for page_template() || gutenberg || plugin 
+  wp_register_style($handle = 'style-css', get_stylesheet_uri(), [], $ver = filemtime($tmp_dir), $media= 'all');
+  wp_register_script($handle = 'main-js', get_template_directory_uri().'/assets/main.js', [], $ver = filemtime($tmp2), $in_footer=true);
+  //
+  // 
+  wp_enqueue_style($handle='style-css');
+  wp_enqueue_script($handle='main-js');
 }
 
  add_action('wp_enqueue_scripts', 'cgr_awpt');
