e84af4eb9fcec3906e7cb8a300788c049fb7e6ec
Cloudbop <boppincolin@gmail.com>
2020-08-26 22:05:12 +0100

    implemented index Loop. fixed bug with nav container see diff

--- index.php
+++ index.php
@@ -10,8 +10,26 @@
 
 <?php get_header();?>
 
-<div class="content">
-  <h1>Hello WordPress</h1>
+<div id="primary">
+  <main id="main" class="site-main mt-5" role="main">
+
+    <?php if ( have_posts() ) { ?>
+      
+      <div class="container">
+        <?php 
+          while ( have_posts() ) : the_post();
+
+            the_title();
+            the_content();
+
+          endwhile;
+        ?>
+      </div>
+    
+    <?php }?>
+
+
+  </main>
 </div>
 
 <?php 
