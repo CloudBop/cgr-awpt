a3180b204e891028d2ef4dcca14bc0b0c2442105
Cloudbop <boppincolin@gmail.com>
2020-08-27 13:16:48 +0100

    implemented template-parts/blog/entry-meta & added new tempate-tag fn for <time> tags+ SEO

--- inc/helpers/template-tags.php
+++ inc/helpers/template-tags.php
@@ -31,3 +31,31 @@ function the_post_custom_thumbnail($post_id, $size = 'featured-thumbnail', $addi
   //
   echo get_the_post_custom_thumbnail($post_id, $size, $additional_attributes);
 }
+
+function cgr_awpt_posted_on() {
+  // using <time> better for screen readers
+  $time_string = '<time class="entry-date published updated" datetime="%1$s"> %2$s </time>';
+
+  // post is modified ( when post published time !== to post modified time )
+  if ( get_the_time($d='U') !== get_the_modified_time($d='U' ) ) {
+    $time_string = '<time class="entry-date published" datetime="%1$s"> %2$s </time>';
+    // for better SEO
+    $time_string .= '<time class="updated" datetime="%3$s"> %4$s </time>';
+  }
+
+  $time_string = sprintf( $time_string,
+    // see time formats
+    esc_attr( get_the_date( DATE_W3C ) ),
+    esc_attr( get_the_date() ),
+    esc_attr( get_the_modified_date( DATE_W3C ) ),
+    esc_attr( get_the_modified_date() ),
+  );
+
+  $posted_on = sprintf(
+    // $ctx = ctx info for translators
+    esc_html_x( 'Posted on $s', $ctx='post date', 'cgr-awpt' ),
+    '<a href="'. esc_url( get_permalink() ) .'" rel="bookmark">'. $time_string .'</a>'
+  );
+
+  echo '<span class="posted-one text-secondary">'. $posted_on .'</span>';
+}
--- template-parts/components/blog/entry-meta.php
+++ template-parts/components/blog/entry-meta.php
@@ -4,3 +4,7 @@
  * 
  * @package cgr-awpt
  */
+?>
+<div class="entry-meta mb-3">
+  <?php cgr_awpt_posted_on(); ?>
+</div>
