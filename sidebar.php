05deff9fefaa7e73a523a778c58c101d25d63eef
Cloudbop <boppincolin@gmail.com>
2020-08-28 14:57:23 +0100

    implemented sidebar.php template and invoked in single.php

---
+++ sidebar.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * sidebar template 
+ * 
+ * @package cgr-awpt
+ */
+?>
+
+<aside id="secondary" roll="complementary">
+  <?php dynamic_sidebar('sidebar-1')?>
+</aside>
--- single.php
+++ single.php
@@ -11,27 +11,37 @@
 
 <div id="primary">
   <main id="main" class="site-main mt-5" role="main">
-    <?php if ( have_posts() ) : ?>
-      <div class="container">
+   <div class="container">
+    <div class="row">
+      <div class="col-lg-8 col-md-8- col-sm-12">
+        <?php if ( have_posts() ) : ?>
+          <div class="container">
 
-        <?php if( is_home() && ! is_front_page() ) { ?>
-          <header class="mb-5">
-              <h1 class="page-title screen-reader-text">
-                <?php single_post_title(); ?>
-              </h1>
-          </header>
-        <?php } // endif
+            <?php if( is_home() && ! is_front_page() ) { ?>
+              <header class="mb-5">
+                  <h1 class="page-title screen-reader-text">
+                    <?php single_post_title(); ?>
+                  </h1>
+              </header>
+            <?php } // endif
 
-          while ( have_posts() ) : the_post();
-            get_template_part( 'template-parts/content');
-          endwhile;
-          ?>
-      </div>  <!-- end container -->
+              while ( have_posts() ) : the_post();
+                get_template_part( 'template-parts/content');
+              endwhile;
+              ?>
+          </div>  <!-- end container -->
 
-    <?php else : 
-      get_template_part('template-parts/content-none');
-      endif;
-    ?>
+        <?php else : 
+          get_template_part('template-parts/content-none');
+          endif;
+        ?>
+      </div>
+
+      <div class="col-lg-4 col-md-4 col-sm-12">
+          <?php get_sidebar(); ?>
+      </div>
+    </div>
+   </div>
 
     <div class="container">
       <?php
