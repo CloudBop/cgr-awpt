c6bf979a0364afa3a5ec52792c9e215eacb52c9d
Cloudbop <boppincolin@gmail.com>
2020-08-28 14:48:14 +0100

    added class-sidebars for registering theme sidebars

---
+++ inc/classes/class-sidebars.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * registers sidebars
+ * 
+ * @package cgr-awpt
+ */
+// theme namespace
+namespace CGR_AWPT\Inc;
+// import singleton functionality for autoloading
+use CGR_AWPT\Inc\Traits\Singleton;
+//
+class Sidebars {
+  // only instantiate once
+  use Singleton;
+
+  protected function __construct() {
+    //load class
+    //wp_die($message="hello", $title, $args);
+    $this->setup_hooks();
+  }
+
+  protected function setup_hooks() {
+
+    /**
+     * Actions
+     */
+    add_action( 'widgets_init', [$this, 'register_sidebars']);
+
+  }
+
+  public function register_sidebars(){
+    //
+    register_sidebar(array(
+      'name'          => __( 'Sidebar', 'cgr-awpt'),
+      'id'            => 'sidebar-1',
+      'description'   => __( 'Main Sidebar', 'cgr-awpt'),
+      'before_widget' => 'div id="%1$s class="widget widget-sidebar %2$s">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>',
+    ));
+
+    register_sidebar(array(
+      'name'          => __( 'Footer sb', 'cgr-awpt'),
+      'id'            => 'sidebar-2',
+      'description'   => __( 'Footer Sidebar', 'cgr-awpt'),
+      'before_widget' => 'div id="%1$s class="widget widget-footer cell column %2$s">',
+      'after_widget'  => '</div>',
+      'before_title'  => '<h3 class="widget-title">',
+      'after_title'   => '</h3>',
+    ));
+  }
+}
