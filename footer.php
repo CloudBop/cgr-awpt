5e3a84255ec59f09217b3402ae450bd84f996a7f
Cloudbop <boppincolin@gmail.com>
2020-08-28 15:35:31 +0100

    added footer sidebar

--- footer.php
+++ footer.php
@@ -7,15 +7,24 @@
  */
 ?>
 
-
-</div> <!-- end #content.site-content -->
-
-</div> <!-- end #page.site -->
-
 <footer>
-  <h2>footer</h2>
+
+  <h3>footer</h3>
+
+  <?php
+    if (is_active_sidebar('sidebar-2' ) ) {
+    ?>
+      <aside>
+        <?php dynamic_sidebar('sidebar-2') ?>
+      </aside>
+    <?php
+    }
+    ?>
+
 </footer>
 
+</div> <!-- end #content.site-content -->
+</div> <!-- end #page.site -->
 <?php wp_footer(); ?>
 </body>
 </html>
