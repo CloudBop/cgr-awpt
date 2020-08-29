84888646222b444239bcb86f4b2269e9622bb9f2
Cloudbop <boppincolin@gmail.com>
2020-08-29 14:27:03 +0100

    removed all core block-patterns

--- inc/classes/class-cgr-awpt-theme.php
+++ inc/classes/class-cgr-awpt-theme.php
@@ -85,6 +85,9 @@ class CGR_AWPT_THEME {
     //
     add_editor_style('assets/build/css/editor.css');
 
+    // remove all core block patterns
+    // remove_theme_support($feature='core-block-patterns');
+
 
 
 
